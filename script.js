cbd64818246135543f7e4db5c5e2d31c17dce07c
Asdfghjkllkj1 <andrewlegoboy1@gmail.com>
2021-04-02 13:20:49 -0500

    Added features
    Added Level object - collections of rooms in a Grid object with a position argument (where in the level's grid the player is, and where it should render)
    Added Grid object - CodeHS's implementation of Grid is trash, so I decided I'd make my own. Mine can be used in function/class default declarations, while CodeHS needs a separate line to do work.
    Added start_levels, a linked Level object with start_room and level_select
    Changed the way the program displayed - used Level objects instead of Room objects

--- script.js
+++ script.js
@@ -2,6 +2,56 @@ window.onload = function() {
     // Code to integrate JS with HTML^^
     //CLASSES
     /**
+     * @description  Essentially a better implementation of CodeHS Grid object - allows for usage in default function/class parameters
+     * @class Grid
+     * @param {list[2]} dimensions       A 2-item list for the dimensions of the generated 2D array (grid)
+     * @param {list[list]} prefill_data  An optional argument that prefills the generated grid with data (MUST BE 2D array structure)
+     */
+    class Grid {
+        constructor(dimensions, prefill_data = undefined) {
+                this.dimensions = dimensions;
+                // dimensions for the 2d array (width,height format)
+                this.data = Array.from(Array(dimensions[0]), () => new Array(dimensions[1]));
+                // makes a 2d array w/ dims from dimensions
+                if (prefill_data != undefined) {
+                    for (var i = 0; i < dimensions[0]; i++) {
+                        for (var j = 0; j < dimensions[1]; j++) {
+                            if (prefill_data[i][j] != undefined) {
+                                this.data[i][j] = prefill_data[i][j];
+                            }
+                        }
+                    }
+                } // this code looks disgusting :/ make more efficient
+            }
+            /**
+             * @description  A function that checks whether a position is index-able within a grid
+             * @function inBound
+             * @param {list[2]} pos  The position to be checked
+             * @returns 
+             */
+        inBound(pos) {
+                return check_bound([0, 0, this.dimensions[0], this.dimensions[1]], pos);
+            }
+            /**
+             * @description  A function that sets a value inside a grid to another value
+             * @function set
+             * @param {list[2]} pos  The position inside the grid to change to val
+             * @param {any} val      The value to change pos to
+             * @returns 
+             */
+        set = (pos, val) => this.data[pos[0], pos[1]] = val;
+        /**
+         * @description  A function taht returns the value of a specific position inside the grid
+         * @function get
+         * @param {list[2]} pos  The position of the desired value inside the grid
+         * @returns 
+         */
+        get(pos) {
+            return this.data[pos[0], pos[1]];
+        }
+    }
+
+    /**
      * @description A general container for room objects
      * @class Room
      * @param {string} name          A name to be displayed for debug/user interfacing purposes
@@ -13,7 +63,7 @@ window.onload = function() {
     class Room {
         constructor(name = 'Room Object', scene_data = [{
                 type: 'web_image',
-                location: [0, 0],
+                location: [50, 50],
                 url: 'https://codehs.com/uploads/978cbcc781e32db59711ed28ad170591',
                 set_size: [100, 100]
             }], action_points = {}, linked_overlay) {
@@ -30,7 +80,7 @@ window.onload = function() {
                 // for linking room data to overlays to be displayed with the room
             }
             /**
-             * @description Displays the corresponding scene of the room; Removes all objects already on the room
+             * @description  Displays the corresponding scene of the room; Removes all objects already on the room
              * @function display
              * @param {bool} overlay  If the room is being displayed as an overlay to something else
              */
@@ -41,22 +91,59 @@ window.onload = function() {
                 load_scene(this.scene_data);
             }
             /**
-             * @description Checks if a position hits an action point
+             * @description  Checks if a position hits an action point
              * @function update
-             * @param {list[2]} pos A position (usually of mouse) to check whether it hits action points or not
+             * @param {list[2]} pos  A position (usually of mouse) to check whether it hits action points or not
              */
         update = pos => {
             for (var action_bound in this.action_points) {
-                if (check_bound(eval(action_bound), pos) == true) { eval(this.action_points[action_bound]) }
+                if (check_bound(eval(action_bound), pos) == true) {
+                    eval(this.action_points[action_bound]);
+                }
             }
         };
     }
 
+    /**
+     * @description  A container for linking Room objects
+     * @class Level
+     * @param {string} name          The name for the entire level
+     * @param {Grid} rooms           A grid (2D array) of Room objects
+     * @param {list[2]} pos  The position of the player (and the position of the room to display)
+     */
+    class Level {
+        constructor(name, rooms = undefined, pos = [0, 0]) {
+                this.name = name;
+                // the name of the level
+                if (rooms == undefined) {
+                    this.rooms = new Grid([1, 1], new Room(linked_overlay = new Room()));
+                } else {
+                    this.rooms = rooms;
+                }
+                // rooms is a grid of Room objects
+                this.pos = pos;
+                // the position at which the player is at
+                this.loaded_room = this.rooms.data[this.pos[0]][this.pos[1]];
+                // room loaded associated with the level
+            }
+            /**
+             * @description  A function that updates the scene based on coordinates and moves the position of the loaded scene based on dx/dy
+             * @function update
+             * @param {int} dx  Move x by dx amount
+             * @param {int} dy  Move y by dy amount
+             */
+        update(dx = 0, dy = 0) {
+            var new_pos = [this.pos[0] + dy, this.pos[1] + dx];
+            if (this.rooms.inBound(new_pos)) this.pos = new_pos;
+            this.loaded_room = this.rooms.data[this.pos[0]][this.pos[1]];
+        }
+    }
+
     //FUNCTIONS
     /**
-     * @description Loads a scene based on scene data
+     * @description  Loads a scene based on scene data
      * @function load_scene
-     * @param {list} raw_data A list of objects telling the function how to load the scene
+     * @param {list} raw_data  A list of objects telling the function how to load the scene
      */
     function load_scene(raw_data) {
         raw_data.forEach(function(object, index) {
@@ -149,7 +236,7 @@ window.onload = function() {
      */
 
     /**
-     * @description Checks if a coordinate falls within a 4-coordinate boundary
+     * @description  Checks if a coordinate falls within a 4-coordinate boundary
      * @function check_bound
      * @param {list[4]} bound  A rectangle boundary to check whether coord is within
      * @param {list[2]} coord  A coordinate to check if it's in bound's rectangle
@@ -160,10 +247,9 @@ window.onload = function() {
         return (bound[0] <= coord[0] && coord[0] <= bound[2]) && (bound[1] <= coord[1] && coord[1] <= bound[3]);
     }
 
-
     /**
-     * @whyimadethis got lazy writing "document.getElementById..." all the time
-     * @description Changes HTML tag text by IDs
+     * @whyimadethis  got lazy writing "document.getElementById..." all the time
+     * @description  Changes HTML tag text by IDs
      * @function changeHTML
      * @param {string} id        The ID of the HTML tag to be changed
      * @param {string} new_text  The text to change to
@@ -173,31 +259,36 @@ window.onload = function() {
     }
 
     //GLOBAL VARIABLES
-    var WIDTH = getWidth();
-    var HEIGHT = getHeight();
+    const WIDTH = getWidth();
+    const HEIGHT = getHeight();
     var USER_NAME;
+    var loaded_room;
+    var LLevel;
     //GAME SETUP
     //TODO: set up room objects for all rooms in game
-    var loaded_room;
-    var start_room = new Room('start_room', [{
+    //To Do: Change the need for "LLevel = __level-name__"; LLevel.pos = [0,0]; LLevel.update();" in action points for rooms
+    const start_room = new Room('start_room', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/67b3fe870835f1df7d063cd6d9a1f654'
-    }], { '[75,144,255,255]': 'USER_NAME=prompt("What is your name?","User000");loaded_room=level_select' });
-    var level_select = new Room('level_select', [{
+    }], { '[75,144,255,255]': 'USER_NAME=prompt("What is your name?","User000");LLevel.update(1,0)' });
+    const level_select = new Room('level_select', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/17a255ac46fcbe31d6789d986d98af8c'
     }], {
-        '[10,100,75,165]': 'loaded_room=L1e;',
-        '[90,100,160,165]': 'loaded_room=L2e;',
-        '[175,100,240,170]': 'loaded_room=L3e;',
-        '[250,100,320,170]': 'loaded_room=L4e;',
-        '[5,190,95,260]': 'loaded_room=L5e;',
-        '[90,190,165,260]': 'loaded_room=L6e;',
-        '[180,190,235,270]': 'loaded_room=L7e;',
-        '[250,200,325,260]': 'loaded_room=L8e;'
+        '[10,100,75,165]': 'LLevel = Level1; LLevel.pos = [0,0];LLevel.update();',
+        '[90,100,160,165]': 'LLevel.loaded_room=L2e;',
+        '[175,100,240,170]': 'LLevel.loaded_room=L3e;',
+        '[250,100,320,170]': 'LLevel.loaded_room=L4e;',
+        '[5,190,95,260]': 'LLevel.loaded_room=L5e;',
+        '[90,190,165,260]': 'LLevel.loaded_room=L6e;',
+        '[180,190,235,270]': 'LLevel.loaded_room=L7e;',
+        '[250,200,325,260]': 'LLevel.loaded_room=L8e;'
     }, new Room('level_overlay'));
+    const LevelStart = new Level('LStart', new Grid([1, 2], [
+        [start_room, level_select]
+    ]), pos = [0, 0]);
     /* 
     template for rooms with only url
     new Room('__name__', [{
@@ -211,71 +302,74 @@ window.onload = function() {
         location: [0, 0],
         url: 'https://codehs.com/uploads/bef060442d7fb97e5775a3b5e953926c'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel=LevelStart;LLevel.pos = [0,0];LLevel.update();'
     });
+    const Level1 = new Level('L1', new Grid([1, 1], [
+        [L1e]
+    ]), pos = [0, 0]);
     var L2e = new Room('Lv2_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/c75b69cd74f41b1c690b981edd438771'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L3e = new Room('Lv3_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/8a0ff0269c38c322e4dd54928a37e1e8'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L4e = new Room('Lv4_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/3f668e244b2c250c27b96f41d8494c07'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L5e = new Room('Lv5_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/423a283535121945fcdc926c85febd0e'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L6e = new Room('Lv6_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/3ce76bc733da05177532fbd004dd250d'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L7e = new Room('Lv7_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/fe193d750c155a8d367b45ee5537b916'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
     var L8e = new Room('Lv8_entrance', [{
         type: 'web_image',
         location: [0, 0],
         url: 'https://codehs.com/uploads/779ee02bf2672c86766750985b3db283'
     }], {
-        '[0,0,400,500]': 'loaded_room=start_room;'
+        '[0,0,getWidth(),getHeight()]': 'LLevel.loaded_room=start_room;'
     });
-    loaded_room = start_room;
+    LLevel = LevelStart;
     //MAIN LOOP
-    loaded_room.display(); // just to display it the first time
-    document.getElementById('current_doc').innerHTML = loaded_room.name;
+    LLevel.update()
+    LLevel.loaded_room.display();
     mouseClickMethod(function(e) {
         // optional: add code that runs every time a click (not necessarily one to do something) happens
-        loaded_room.update([e.getX(), e.getY()]);
-        changeHTML('current_doc', loaded_room.name); // debug purposes, displays room name in h2
-        changeHTML('username', USER_NAME); // debug purposes, displays username
-        loaded_room.display();
-        loaded_room.linked_overlay.display(overlay = true);
+        LLevel.loaded_room.update([e.getX(), e.getY()]);
+        LLevel.loaded_room.display();
+        document.getElementById('current_doc').innerHTML = LLevel.loaded_room.name;
+        changeHTML('text', LLevel.name);
+        changeHTML('text2', LLevel.pos);
     }); // Updates the loaded room on mouse click; displays loaded room if there is a different one being loaded
     mouseMoveMethod(function(e) {
-        changeHTML(`mouse pos: ${e.getX()}, ${e.getY()}`); // displays mouse current pos in h3
+        changeHTML('mouse_pos', `${e.getX()}, ${e.getY()}`); // displays mouse current pos in h3
     }); // mouse pos display; mainly for debug
     //HTML Integration
     if (typeof start === 'function') {
